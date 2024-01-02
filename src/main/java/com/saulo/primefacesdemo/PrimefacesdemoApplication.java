d592cca83ccb5192ede0d6c2e0eb3a3695cd960f
Saulo0097 <saulohermida1997@gmail.com>
2024-01-02 13:45:21 -0500

    new demo primefaces

---
+++ src/main/java/com/saulo/primefacesdemo/PrimefacesdemoApplication.java
@@ -0,0 +1,13 @@
+package com.saulo.primefacesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefacesdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimefacesdemoApplication.class, args);
+    }
+
+}
