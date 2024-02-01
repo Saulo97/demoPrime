aae5d2bf593867dc44a1bc830407dfcd1e8461f3
Saulo.0097 <39831129+Saulo97@users.noreply.github.com>
2024-02-01 15:12:15 -0500

    Update MVCConfig.java

--- src/main/java/com/saulo/primefacesdemo/MVCConfig.java
+++ src/main/java/com/saulo/primefacesdemo/MVCConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MVCConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("**").setViewName("index.xhtml")
         registry.addViewController("/").setViewName("index.xhtml");
     }
 }
